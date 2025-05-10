6eda60037954d6885a9374d0a38781df5d2f2854
Tony Oliver <nphuoclong87@gmail.com>
2025-05-10 20:40:16 +0700

    feat(post-service): add getAllPosts endpoint with pagination, Redis caching and update handler for rate Limiter

--- post-service/src/controllers/post-controller.js
+++ post-service/src/controllers/post-controller.js
@@ -5,6 +5,7 @@ const {
 const logger = require("../utils/logger");
 const { validateCreatePost } = require("../utils/validation");
 const Post = require("../models/Post");
+const redisClient = require("../utils/redis");
 
 const createNewPost = asyncHandler(async (req, res) => {
   logger.info("Create post endpoint hit...");
@@ -34,4 +35,42 @@ const createNewPost = asyncHandler(async (req, res) => {
   });
 });
 
-module.exports = { createNewPost };
+const getAllPosts = asyncHandler(async (req, res) => {
+  logger.info("Get all posts hit...");
+
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const startIndex = (page - 1) * limit;
+
+  const cachedKey = `posts:page:${page}:limit:${limit}`;
+  const cachedPosts = await redisClient.get(cachedKey);
+
+  if (cachedPosts) {
+    return res.status(200).json({
+      posts: JSON.parse(cachedPosts),
+    });
+  }
+
+  const posts = await Post.find({})
+    .sort({ createdAt: -1 })
+    .skip(startIndex)
+    .limit(limit)
+    .lean();
+
+  const totalPosts = await Post.countDocuments();
+  const totalPages = Math.ceil(totalPosts / limit);
+
+  const result = {
+    currentPage: page,
+    totalPosts,
+    totalPages,
+    posts,
+    limit,
+  };
+
+  await redisClient.setex(cachedKey, 300, JSON.stringify(result));
+
+  return res.status(200).json(result);
+});
+
+module.exports = { createNewPost, getAllPosts };
--- post-service/src/routes/post-routes.js
+++ post-service/src/routes/post-routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const authenticateRequest = require("../middleware/authMiddleware");
-const { createNewPost } = require("../controllers/post-controller");
+const {
+  createNewPost,
+  getAllPosts,
+} = require("../controllers/post-controller");
 
 const router = express.Router();
 
 router.use(authenticateRequest);
 
 router.post("/create-post", createNewPost);
+router.get("/all-posts", getAllPosts);
 
 module.exports = router;
